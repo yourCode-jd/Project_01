17c7d2a014c501815e80692a1705c9751939265e
jaspreet tpss <jaspreetstpss@gmail.com>
2024-10-14 19:58:30 +0530

    create header section

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,10 +1,27 @@
-
-import './App.css';
+import "./App.css";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Blogs from "./Pages/Blogs";
+import Services from "./Pages/Services";
+import Contact from "./Pages/Contact";
+import Navbar from "./Components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-<>
-</>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path="Blogs" element={<Blogs />} />
+            <Route path="Contact" element={<Contact />} />
+            <Route path="Services" element={<Services />} />
+            <Route path="About" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
